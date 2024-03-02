71eb09d3d2334e918583f7486cbdfe3136bf8269
Caeser9 <kaycerkhouini86@outlook.fr>
2024-03-02 13:12:10 +0100

    first commit

---
+++ CourzeloProject/src/main/java/com/example/courzeloproject/CourzeloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.courzeloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourzeloProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourzeloProjectApplication.class, args);
+    }
+
+}
